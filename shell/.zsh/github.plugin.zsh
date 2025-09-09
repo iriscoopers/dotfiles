f79dedfdc1286acb627db6a67303e830b47ed26f
iriscoopers <bighairamazon@gmail.com>
2025-09-09 10:22:59 +0200

    Fix missing parts

--- shell/.zsh/github.plugin.zsh
+++ shell/.zsh/github.plugin.zsh
@@ -8,5 +8,16 @@ alias ghic='gh issue create'
 
 # Set GitHub CLI editor if not already set
 if [ -z "$GH_EDITOR" ]; then
-  export GH_EDITOR='vim'
+  export GH_EDITOR='nvim'
 fi 
+
+gfzf() {
+  gh pr list -S $1 | fzf | awk '{print $1}'	fi 
+}	
+
+# $1 = db action; setup or migrate will become db:setup or db:migrate	
+gmu() {	
+  dbaction="db:$1"	
+
+  gl && [ -f Gemfile.lock ] && bundle _$(grep -A 1 "BUNDLED WITH" Gemfile.lock | grep -v "BUNDLED WITH" | awk '{$1=$1};1')_ install && be rails $dbaction && [ -f yarn.lock ] && yarn	
+}	
